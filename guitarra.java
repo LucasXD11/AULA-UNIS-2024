17effaf3f2cda478d31eb782553a2b60bad0e091
LucasXD11 <168382689+LucasXD11@users.noreply.github.com>
2024-05-08 10:57:21 -0300

    Update guitarra.java

--- guitarra.java
+++ guitarra.java
@@ -1,1 +1,55 @@
-
+package unidade_1;
+//declaração da classe
+public class Guitarra (
+private String numeroserie, fabricante, modelo, tipo, madeira;
+privats double preco:
+//Criação do construtor de classe Guitarra
+public Guitarra(
+String numeroserie, String fabricante,
+String modelo, String madeira, String tipo, double prece) (
+this.numeroseris numeroserie:
+this.fabricante fabricante;
+this modele modelo;
+this tipo tipo;
+this madeing madeira;
+this.preco preco:
+}
+public String getNumeroseric() {
+return numeroserie:
+}
+public void setNumero Serie(String numeroserie) {
+this.numeroserie numeroserie:
+}
+public String getfabricante() (
+return fabricante;
+}
+public void setfabricante (String fabricante) { this fabricante fabricante:
+public String getModelo(){
+return modelo;
+public void sethodelo(String modelo) (
+this modelo modelo;
+}
+public String getline() {
+return tipo:
+}
+public void setling(String tipo) {
+this tipo tipo;
+}
+public String gettiadsica() {
+cotuco madeira;
+}
+public void setMadeira (String madeira)
+{
+this.madeira madeira;
+}
+public double getPreco() {
+Esturn prese:
+public void setPress(double neese) {
+this.preco preco:
+}
+//o método maial) matada principal da classe
+public static void main(String[] args) {
+//instanciamos un objeto chamado minhasustora" que será do tipo "Guitarra"
+//0s valores passados são usados pelo construtor da classe para criar
+//o objeto
+Guitarra minhaGuitarra new Guitarra ("@1028384", "fender", "telecaster", "elétrica", "mogno", 1500);
